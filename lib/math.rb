2705c959d7f9a8542ea41e0267bcca84c5022abb
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-10 15:56:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,11 @@ num1+num2
 end
 
 def subtraction(num1, num2)
-num2-num1
+num1-num2
 end
 
 def division(num1, num2)
-num2/num1
+num2 / num1
 end
 
 def multiplication(num1, num2)
@@ -15,7 +15,7 @@ num1*num2
 end
 
 def modulo(num1, num2)
-num2%num1
+num2 % num1
 end
 
 def square_root(num)
